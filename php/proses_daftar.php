ffa457a513cbda287983d35d6d1b841f4c8b3fdf
Faradhibaumardhani <faradhibaumardhani@gmail.com>
2024-11-28 15:06:13 +0800

    proses_daftar.php

--- php/proses_daftar.php
+++ php/proses_daftar.php
@@ -1,14 +1,12 @@
 <?php
-include 'koneksi.php'; // Menyambungkan ke database
+include 'koneksi.php';  
 
-// Tangkap data dari form
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Hash password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Periksa apakah email sudah ada
+
 $query = "SELECT * FROM sign_up WHERE email = ?";
 $stmt = mysqli_prepare($koneksi, $query);
 mysqli_stmt_bind_param($stmt, "s", $email);
@@ -19,7 +17,6 @@ if (mysqli_num_rows($result) > 0) {
     echo "Email sudah digunakan.";
     echo '<br><a href="../sign_in.html">Login</a>';
 } else {
-    // Insert data baru
     $insert_query = "INSERT INTO sign_up (email, password) VALUES (?, ?)";
     $insert_stmt = mysqli_prepare($koneksi, $insert_query);
     mysqli_stmt_bind_param($insert_stmt, "ss", $email, $hashed_password);
