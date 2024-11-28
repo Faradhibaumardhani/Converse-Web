aded2f027c6a90306cf035ce00a9516c5aa68b15
Faradhibaumardhani <faradhibaumardhani@gmail.com>
2024-11-28 15:07:09 +0800

    proses_login.php

--- php/proses_login.php
+++ php/proses_login.php
@@ -1,11 +1,9 @@
 <?php
-include 'koneksi.php'; // Menyambungkan ke database
+include 'koneksi.php';  
 
-// Tangkap data dari form
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Query untuk memeriksa email dan password
 $query = "SELECT * FROM sign_up WHERE email = ?";
 $stmt = mysqli_prepare($koneksi, $query);
 mysqli_stmt_bind_param($stmt, "s", $email);
@@ -15,19 +13,18 @@ $result = mysqli_stmt_get_result($stmt);
 if (mysqli_num_rows($result) > 0) {
     $user = mysqli_fetch_assoc($result);
 
-    // Verifikasi password
     if (password_verify($password, $user['password'])) {
         session_start();
         $_SESSION['user_id'] = $user['id'];
         echo "Login berhasil!";
-        echo '<br><a href="../home.html">Kembali ke Beranda</a>'; // Link kembali ke halaman utama
+        echo '<br><a href="../home.html">Kembali ke Beranda</a>'; 
     } else {
         echo "Password salah.";
-        echo '<br><a href="../sign_in.html">Kembali ke Beranda</a>'; // Link kembali ke halaman utama
+        echo '<br><a href="../sign_in.html">Kembali ke Beranda</a>'; 
     }
 } else {
     echo "Email tidak ditemukan.";
-    echo '<br><a href="../sign_up.html">Kembali ke Beranda</a>'; // Link kembali ke halaman utama
+    echo '<br><a href="../sign_up.html">Kembali ke Beranda</a>'; 
 }
 ?>
 
